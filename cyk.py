1e7e7277c9d59fcf7228ce5042f3d7d9d7e1333a
louissmit <ghr1Co74G9>
2014-03-24 12:17:26 +0100

    dual word alleviance

--- cyk.py
+++ cyk.py
@@ -86,7 +86,7 @@ def run_parser(corpus):
 		parser = Parser(grammar)
 		sent = splitSentence(sentence)
 		tree = parser.parse(sent)
-		tree.draw()
+		# tree.draw()
 		# print tree.pprint(margin=30)
 		extractDepParse(tree, sentence)
 
@@ -94,31 +94,34 @@ def run_parser(corpus):
 
 def extractDepParse(tree, sentence):
 	"""
-	Transforms a PCFG parsed tree in DMV split head encoding back to a dependency definition
+	Transforms a PCFG parsed tree in DMV split head encoding back to a dependency definition.
+	Has issues with the same word occuring twice in the sentence :/
 	@param tree: NLTK tree
 	@param sentence: The original input sentence
 	"""
 	tree = tree.pprint(margin=30)
 	deps = [None for i in xrange(0, len(sentence))]
-	last_index = {}
+	already_assigned = {}
 	stack = []
 	# I could write a comment explaining this code, but this code is very dumb and probably wrong so I don't expect you to understand
 	for line in tree.split('\n'):
 		# indents = len(re.findall('[ ]*', line)[0])
+		if line.strip()[0:3] == '(S':
+			stack.append(0)
 		if line.strip()[2:4] == '1_':
-			stack.append(line.strip()[4:])
+			dep = line.strip()[4:]
+			dep_index = sentence.index(dep) + 1
+			stack.append(dep_index)
 		if line.strip()[0:3] == '(Y_' and len(stack) > 0:
-			y = re.findall('Y_[^ ]*', line)[0][2:].strip()
-			dep = stack.pop()
-			if not last_index.has_key(y):
-				last_index[y] = 0
-			if not last_index.has_key(dep):
-				last_index[dep] = 0
-			dep_index = sentence.index(dep, last_index[dep])
-			y_index = sentence.index(y, last_index[y])
-			deps[y_index] = dep_index + 1
-			last_index[dep] = dep_index
-			last_index[y] = y_index
+			y = line.strip()[3:]
+			dep_index = stack.pop()
+			y_index = sentence.index(y)
+			if already_assigned.has_key(y_index):
+				y_index = sentence.index(y, y_index+1)
+			else:
+				already_assigned[y_index] = True
+
+			deps[y_index] = dep_index
 
 	for i, word in enumerate(sentence):
 		print word, deps[i]
