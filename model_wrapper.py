a7a95ec60bc2f5b285c33ed676d53abf89db60cf
louissmit <ghr1Co74G9>
2014-03-24 19:55:27 +0100

    matrices are not aligned

--- model_wrapper.py
+++ model_wrapper.py
@@ -10,7 +10,7 @@ import numpy as np
 from sklearn import mixture
 
 class model_wrapper:
-    
+
     def __init__(self, k=200):
         print 'Loading model data - this may take a while'
         self.GMM = pickle.load(open('GMM'+str(k),'rb'))
@@ -18,15 +18,16 @@ class model_wrapper:
         self.embeddings = loadEmbeddings()
         self.root_weights = pickle.load(open('root'+str(k),'rb'))
         print 'Model data loaded'
-        
-        
+
+
     def getRootProb(self, argument):
         if self.embeddings.has_key(argument):
             probs = getGMMProbs(self.GMM, argument, self.embeddings)
+            print np.shape(probs), np.shape(self.root_weights)
             return np.dot(probs,self.root_weights)
         else:
             return 0
-        
+
     def getProb(self, head, argument, val=0, direction='left'):
         if val==1 and direction == 'left':
             return getProb1(head, argument, self.GMM, self.rep_vecs[0], self.embeddings)
@@ -38,39 +39,39 @@ class model_wrapper:
             return getProb1(head, argument, self.GMM, self.rep_vecs[3], self.embeddings)
         else:
             print 'Something went wrong - cannot obtain a probability, returning 0'
-            
-            
+
+
     def findBestArgs(self, head, model=0):
         probs = list()
         for arg in self.embeddings.keys():
             probs.append(getProb1(head,arg,self.GMM, self.rep_vecs[model],self.embeddings))
-            
+
         best = np.argsort(probs)[-10:]
-        
+
         out = list()
         for b in best:
             out.append((self.embeddings.keys()[b],probs[b]))
-        
+
         return out
-        
-        
-        
-        
+
+
+
+
 def getGMMProbs(GMM,argument,embeddings):
     #logs = mixture.log_multivariate_normal_density(np.array([embeddings[argument]]), GMM.means_, GMM.covars_, GMM.covariance_type)[0] + np.log(GMM.weights_)
-    
+
     X = np.array([embeddings[argument]])
-    
+
     lpr = (mixture.log_multivariate_normal_density(X, GMM.means_, GMM.covars_,
                                                GMM.covariance_type))
-               
+
     probs = np.exp(lpr)
-    
-    
+
+
     return probs[0]
-    
-    
-    
+
+
+
 def getProb1(head, argument, GMM, rep_vec, embeddings):
     probs = getGMMProbs(GMM,argument,embeddings)
     if rep_vec.has_key(head):
@@ -78,7 +79,7 @@ def getProb1(head, argument, GMM, rep_vec, embeddings):
     else:
         #print head, 'doesnt have this kind of dep, returning 0'
         return 0
-        
+
 def getProb2(head, argument, GMM, rep_vec, embeddings):
     #probs = getGMMProbs(GMM,argument,embeddings)
     if rep_vec.has_key(head):
@@ -88,19 +89,18 @@ def getProb2(head, argument, GMM, rep_vec, embeddings):
         return np.dot(probs,rep_vec[head])[0]
     else:
         return 0
-        
-        
-        
-        
+
+
+
+
 def loadEmbeddings():
     #loads the word embeddings as a dictionary
-    embeds = dict()    
-    
+    embeds = dict()
+
     A = open('embeddings-scaled.EMBEDDING_SIZE=50.txt','r')
     for word in A:
         w = word.split()
         #normalizing to unit length
         embeds[w[0]] = np.double(w[1:])/np.linalg.norm(np.double(w[1:]))
-        
+
     return embeds
-    
