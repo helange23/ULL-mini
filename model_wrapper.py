556202a1d303131c67eeb0fde995e4d4b0edc33e
henning <helange23@gmail.com>
2014-03-24 12:42:42 +0100

    now

--- model_wrapper.py
+++ model_wrapper.py
@@ -40,7 +40,7 @@ class model_wrapper:
     def findBestArgs(self, head, model=0):
         probs = list()
         for arg in self.embeddings.keys():
-            probs.append(getProb(head,arg,self.GMM, self.rep_vec[model],self.embeddings))
+            probs.append(getProb(head,arg,self.GMM, self.rep_vecs[model],self.embeddings))
             
         best = np.argsort(probs)[-10:]
         
